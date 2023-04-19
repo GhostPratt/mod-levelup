bec40449d583499241a5929ca40e2555689aa90d
phill <phillipprtt@gmail.com>
2023-04-19 14:32:11 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,25 +1,32 @@
-# SKELETON - Module template
+# Level Up Chaos Rewards
 
-[English](README.md) | [Español](README_ES.md)
+## Description
 
-
-## How to create your own module
-
-1. Use the script `create_module.sh` located in [`modules/`](https://github.com/azerothcore/azerothcore-wotlk/tree/master/modules) to start quickly with all the files you need and your git repo configured correctly (heavily recommended).
-1. You can then use these scripts to start your project: https://github.com/azerothcore/azerothcore-boilerplates
-1. Do not hesitate to compare with some of our newer/bigger/famous modules.
-1. Edit the `README.md` and other files (`include.sh` etc...) to fit your module. Note: the README is automatically created from `README_example.md` when you use the script `create_module.sh`.
-1. Publish your module to our [catalogue](https://github.com/azerothcore/modules-catalogue).
+This module will reward your players everytime they level up with a random item from the Database.
+This is part 1 of some upcoming Chaos focused mods.
 
 
-## How to test your module?
+## How to use ingame
 
-Disable PCH (precompiled headers) and try to compile. To disable PCH, set `-DNOPCH=1` with Cmake (more info [here](http://www.azerothcore.org/wiki/CMake-options)).
+Levelup and get some rewards!
 
-If you forgot some headers, it is time to add them!
 
-## Licensing
+## Requirements
 
-The default license of the skeleton-module template is the MIT but you can use a different license for your own modules.
+My_new_module requires:
 
-So modules can also be kept private. However, if you need to add new hooks to the core, as well as improving existing ones, you have to share your improvements because the main core is released under the AGPL license. Please [provide a PR](https://www.azerothcore.org/wiki/How-to-create-a-PR) if that is the case.
+- AzerothCore v4.0.0+
+
+
+## Installation
+
+```
+1) Simply `git clone` the module under the `modules` directory of your AzerothCore source or copy paste it manually.
+2) Import the SQL manually to the right Database (auth, world or characters) or with the `db_assembler.sh` (if `include.sh` provided).
+3) Re-run cmake and launch a clean build of AzerothCore.
+```
+
+## Credits
+
+* [GhostPratt](https://github.com/GhostPratt) (author of the module)
+* AzerothCore: [repository](https://github.com/azerothcore) - [website](http://azerothcore.org/) - [discord chat community](https://discord.gg/PaqQRkd)
---
+++ src/levelup.cpp
@@ -0,0 +1,163 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ */
+
+#include "list"
+#include "string"
+#include "random"
+
+using namespace std;
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Config.h"
+#include "Chat.h"
+
+using namespace Acore::ChatCommands;
+
+// Add player scripts
+class levelup : public PlayerScript
+{
+public:
+    levelup() : PlayerScript("levelup") {}
+
+    void OnLogin(Player* player) override
+    {
+
+    }
+    void OnLevelChanged(Player* player, uint8 oldLevel) override
+    {
+
+        QueryResult result = WorldDatabase.Query("SELECT entry FROM item_template");
+
+        list<int> itemlist;
+        if (result)
+        {
+            do
+            {
+                int entry = result->Fetch()[0].Get<uint32>();
+                itemlist.emplace_back(entry);
+            } while (result->NextRow());
+        }
+
+        int size = itemlist.size();
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> distr(0, size);
+        auto itemlistFront = itemlist.begin();
+        advance(itemlistFront, distr(gen));
+        int item = *itemlistFront;
+
+        QueryResult stackResult = WorldDatabase.Query("SELECT stackable FROM item_template as t WHERE t.entry = " +
+                                                      to_string(item));
+
+        list<int> stackList;
+        if (stackResult)
+        {
+            do
+            {
+                int entry = stackResult->Fetch()[0].Get<uint32>();
+                stackList.emplace_back(entry);
+            } while (stackResult->NextRow());
+        }
+        auto stacklistFront = stackList.begin();
+        int stackSize = *stacklistFront;
+
+        player->AddItem(item, stackSize);
+
+        return;
+
+    }
+};
+
+class levelupCommand : public CommandScript
+{
+public:
+    levelupCommand(): CommandScript("levelupCommand") {}
+
+    ChatCommandTable GetCommands() const override
+    {
+        static ChatCommandTable levelupCommandTable =
+                {
+                        {"itemsel", HandlelevelupItemSelCommand, SEC_ADMINISTRATOR, Console::Yes},
+                };
+        static ChatCommandTable levelupCommandBaseTable =
+                {
+                        {"lu", levelupCommandTable},
+                };
+        return levelupCommandBaseTable;
+    }
+
+    static bool HandlelevelupItemSelCommand(ChatHandler *handler)
+    {
+        Player *player = handler->GetSession()->GetPlayer();
+
+        if(!player)
+            return false;
+
+        if (player->IsInCombat())
+        {
+            handler->SendSysMessage("You can't use this command while in combat!");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+        QueryResult result = WorldDatabase.Query("SELECT entry FROM item_template");
+
+        if (!result)
+        {
+            handler->SendSysMessage("Query Failed");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+        list<int> itemlist;
+        if (result)
+        {
+            do
+            {
+                int entry = result->Fetch()[0].Get<uint32>();
+                itemlist.emplace_back(entry);
+            } while (result->NextRow());
+        }
+
+        int size = itemlist.size();
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> distr(0, size);
+        auto itemlistFront = itemlist.begin();
+        advance(itemlistFront, distr(gen));
+        int item = *itemlistFront;
+
+        QueryResult stackResult = WorldDatabase.Query("SELECT stackable FROM item_template as t WHERE t.entry = " +
+                                                              to_string(item));
+        if (!stackResult)
+        {
+            handler->SendSysMessage("Query Failed");
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+        list<int> stackList;
+        if (stackResult)
+        {
+            do
+            {
+                int entry = stackResult->Fetch()[0].Get<uint32>();
+                stackList.emplace_back(entry);
+            } while (stackResult->NextRow());
+        }
+        auto stacklistFront = stackList.begin();
+        int stackSize = *stacklistFront;
+
+        handler->SendSysMessage(std::to_string(item));
+        player->AddItem(item, stackSize);
+
+        return true;
+    }
+};
+
+
+// Add all scripts in one
+void AddlevelupScripts()
+{
+    new levelup();
+    new levelupCommand();
+}
